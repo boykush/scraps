c4b0216ff946164141a93b85f85c6dbf6b594d76
Taichi Kushiro <k.taichi0315@gmail.com>
2023-11-03 15:23:58 +0900

    add slug field in Scrap

--- src/build/model/scrap.rs
+++ src/build/model/scrap.rs
@@ -2,11 +2,12 @@ use std::fmt::Display;
 
 use url::Url;
 
-use crate::libs::markdown;
+use crate::libs::{markdown, slugify};
 
 #[derive(PartialEq, Clone, Debug)]
 pub struct Scrap {
     pub title: Title,
+    pub slug: Slug,
     pub links: Vec<Title>,
     pub html_content: String,
     pub thumbnail: Option<Url>,
@@ -24,6 +25,7 @@ impl Scrap {
 
         Scrap {
             title: Title::new(title),
+            slug: Slug::new(title),
             links: links,
             html_content: html_content,
             thumbnail: thumbnail,
@@ -47,34 +49,29 @@ impl Display for Title {
     }
 }
 
+#[derive(PartialEq, Clone, Debug)]
+pub struct Slug(String);
+
+impl Slug {
+    pub fn new(v: &str) -> Slug {
+        let slug = slugify::by_dash(v);
+        Slug(slug)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_new() {
-        let scrap1 = Scrap::new(
-            "scrap1",
-            "[[link1]] ![](https://example.com/image.png)",
-            &None,
-        );
-        assert_eq!(
-            scrap1,
-            Scrap {
-                title: Title::new("scrap1"),
-                links: vec!(Title::new("link1")),
-                html_content: "<p><a href=\"./link1.html\">link1</a> <img src=\"https://example.com/image.png\" alt=\"\" /></p>\n".to_string(),
-                thumbnail: Some(Url::parse("https://example.com/image.png").unwrap()),
-                commited_ts: None
-            }
-        );
-
-        let mut scrap2 = Scrap::new("scrap2", "[[link1]] [[link2]]", &None);
-        assert_eq!(scrap2.title, Title::new("scrap2"));
-        assert_eq!(scrap2.links.sort(), vec!(Title::new("link1"), Title::new("link2")).sort());
-        assert_eq!(scrap2.html_content, "<p><a href=\"./link1.html\">link1</a> <a href=\"./link2.html\">link2</a></p>\n"
+        let mut scrap = Scrap::new("scrap title", "[[link1]] [[link2]]", &None);
+        assert_eq!(scrap.title, Title::new("scrap title"));
+        assert_eq!(scrap.slug, Slug::new("scrap-title"));
+        assert_eq!(scrap.links.sort(), vec!(Title::new("link1"), Title::new("link2")).sort());
+        assert_eq!(scrap.html_content, "<p><a href=\"./link1.html\">link1</a> <a href=\"./link2.html\">link2</a></p>\n"
                         .to_string());
-        assert_eq!(scrap2.thumbnail, None);
-        assert_eq!(scrap2.commited_ts, None);
+        assert_eq!(scrap.thumbnail, None);
+        assert_eq!(scrap.commited_ts, None);
     }
 }
