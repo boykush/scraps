88de225a832cc7bc5e07f68322bd57c7fca1d13a
Taichi Kushiro <boykush315@gmail.com>
2024-12-24 02:02:27 +0900

    Revert "fix publish-rust-crate actions"
    This reverts commit edd01e4515929f915f14d4229379bf9ab4457bab.

--- src/libs/markdown.rs
+++ src/libs/markdown.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 use pulldown_cmark::{html::push_html, CowStr, Event, LinkType, Options, Parser, Tag};
+use scraps_libs_slugify::slugify;
 use url::Url;
 
 const PARSER_OPTION: Options = Options::all();
