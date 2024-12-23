96a305823e36351e4b0321475333b00d5425cffb
Taichi Kushiro <boykush315@gmail.com>
2024-12-23 18:53:56 +0900

    cargo fmt

--- src/libs/markdown.rs
+++ src/libs/markdown.rs
@@ -2,8 +2,8 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 use pulldown_cmark::{html::push_html, CowStr, Event, LinkType, Options, Parser, Tag};
-use url::Url;
 use scraps_libs_slugify::slugify;
+use url::Url;
 
 const PARSER_OPTION: Options = Options::all();
 
