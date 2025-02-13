987b73e694140d04db226f67dfb893dee1dcfda3
Taichi Kushiro <boykush315@gmail.com>
2025-02-14 01:40:01 +0900

    cargo fmt

--- modules/libs/src/markdown.rs
+++ modules/libs/src/markdown.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 use super::slugify;
 use pulldown_cmark::{
-    html::push_html, CodeBlockKind, CowStr, Event, LinkType, Options, Parser, Tag
+    html::push_html, CodeBlockKind, CowStr, Event, LinkType, Options, Parser, Tag,
 };
 use url::Url;
 
@@ -24,11 +24,14 @@ pub fn head_image(text: &str) -> Option<Url> {
 pub fn extract_link_titles(text: &str) -> Vec<String> {
     let parser = Parser::new_ext(text, PARSER_OPTION);
 
-    let link_titles = parser.flat_map(|event| {
-        match event {
-            Event::Start(Tag::Link { link_type: LinkType::WikiLink { has_pothole: _ }, dest_url: CowStr::Borrowed(dest_url), title: _, id:_}) => Some(dest_url.to_string()),
-            _ => None
-        }
+    let link_titles = parser.flat_map(|event| match event {
+        Event::Start(Tag::Link {
+            link_type: LinkType::WikiLink { has_pothole: _ },
+            dest_url: CowStr::Borrowed(dest_url),
+            title: _,
+            id: _,
+        }) => Some(dest_url.to_string()),
+        _ => None,
     });
 
     let hashed: HashSet<String> = link_titles.into_iter().collect();
@@ -39,20 +42,27 @@ pub fn to_html(text: &str, base_url: &Url) -> String {
     let mut html_buf = String::new();
     let parser = Parser::new_ext(text, PARSER_OPTION);
 
-    let replaced = parser.map(|event| {
-        match event {
-                Event::Start(Tag::Link { link_type: LinkType::WikiLink { has_pothole }, dest_url: CowStr::Borrowed(dest_url), title: CowStr::Borrowed(title), id})
-             => {
-                let slug = slugify::by_dash(dest_url);
-                let link = format!("{base_url}scraps/{slug}.html");
-                let start_link = Event::Start(Tag::Link { link_type: LinkType::WikiLink { has_pothole }, dest_url: link.into(), title: title.into(), id });
-                start_link
-            }
-            Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(CowStr::Borrowed(language)))) => {
-                to_html_code_start_event(language)
-            }
-            e1=> e1.clone(),
+    let replaced = parser.map(|event| match event {
+        Event::Start(Tag::Link {
+            link_type: LinkType::WikiLink { has_pothole },
+            dest_url: CowStr::Borrowed(dest_url),
+            title: CowStr::Borrowed(title),
+            id,
+        }) => {
+            let slug = slugify::by_dash(dest_url);
+            let link = format!("{base_url}scraps/{slug}.html");
+            let start_link = Event::Start(Tag::Link {
+                link_type: LinkType::WikiLink { has_pothole },
+                dest_url: link.into(),
+                title: title.into(),
+                id,
+            });
+            start_link
         }
+        Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(CowStr::Borrowed(language)))) => {
+            to_html_code_start_event(language)
+        }
+        e1 => e1.clone(),
     });
 
     push_html(&mut html_buf, replaced);
@@ -95,7 +105,7 @@ mod tests {
             "last",
             "duplicate",
             "Domain Driven Design",
-            "Test-driven development"
+            "Test-driven development",
         ]
         .iter()
         .map(|s| s.to_string())
