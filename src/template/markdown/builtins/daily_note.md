5c79aad39e74cb2421d2df574c5c72e8458ac3f5
Taichi Kushiro <boykush315@gmail.com>
2025-02-17 01:54:37 +0900

    remove builtins

--- src/template/markdown/builtins/daily_note.md
+++
@@ -1,4 +0,0 @@
-+++
-[template]
-title = "{{ now() | date(timezone=timezone) }}"
-+++
--- src/template/markdown/markdown_tera.rs
+++ src/template/markdown/markdown_tera.rs
@@ -2,15 +2,7 @@ use once_cell::sync::Lazy;
 use scraps_libs::error::{anyhow::Context, ScrapError, ScrapResult};
 use tera::Tera;
 
-static MARKDOWN_TERA: Lazy<Tera> = Lazy::new(|| {
-    let mut tera = Tera::default();
-    tera.add_raw_templates(vec![(
-        "daily_note.md",
-        include_str!("builtins/daily_note.md"),
-    )])
-    .unwrap();
-    tera
-});
+static MARKDOWN_TERA: Lazy<Tera> = Lazy::new(|| Tera::default());
 
 pub fn init(template_dir: &str) -> ScrapResult<(Tera, tera::Context)> {
     let mut tera = Tera::new(template_dir).context(ScrapError::PublicRender)?;
