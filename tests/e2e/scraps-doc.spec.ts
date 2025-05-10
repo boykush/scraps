5ae5f229b2008f2fdc2d0c1fbcf64a3a2d85eaa4
Taichi Kushiro <boykush315@gmail.com>
2025-05-11 00:36:35 +0900

    add test for search scraps

--- tests/e2e/scraps-doc.spec.ts
+++ tests/e2e/scraps-doc.spec.ts
@@ -1,12 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('has metadata', async ({ page }, testInfo) => {
-  await page.goto('http://localhost:1112/');
-  await page.screenshot({
-    path: testInfo.outputPath('home.png'),
-    fullPage: true,
-  });
+test('has title', async ({ page }) => {
+  await page.goto('/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Scraps Doc/);
+});
+
+test('search scraps', async ({ page }) => {
+  await page.goto('/');
+
+  // Fill the [id="search-input"] input.
+  await page.locator('[id="search-input"]').fill('What is');
+
+  // Press Enter.
+  await page.keyboard.press('Enter');
+
+  // Expect the search results to contain "What is Scraps?".
+  const searchResults = await page.locator('[id="search-results"]').innerHTML();
+  expect(searchResults).toContain('What is Scraps?');
 });
